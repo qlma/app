3bb0d598a83299d71bac324f76f52c70c53a1d5c
Jouni Leino <jouni.leino@siili.com>
2020-06-19 00:18:38 +0200

    Adding missing urls.py

---
+++ users/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('add_user', views.add_user, name="add_user"),
+    path('add_user_save', views.add_user_save, name="add_user_save"),
+    path('manage_users', views.manage_users, name="manage_users"),
+    path('edit_user/<str:user_id>', views.edit_user, name="edit_user"),
+    path('edit_user_save', views.edit_user_save, name="edit_user_save"),
+
+]
