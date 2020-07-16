d20e77a40685f687af80e83cedb45d832be598f6
Jouni Leino <jouni.leino@siili.com>
2020-07-16 21:10:46 +0200

    changing admin and static

--- .platform/hooks/predeploy/01_migrations.sh
+++ .platform/hooks/predeploy/01_migrations.sh
@@ -4,6 +4,6 @@ source /var/app/venv/*/bin/activate
 cd /var/app/current
 
 python manage.py makemigrations
+python manage.py createsuperuser
 python manage.py migrate
-#python manage.py createsuperuser
 python manage.py collectstatic --noinput
