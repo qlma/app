f5f4f40ddf0a59a8c30b6cd05969d31cc3b05cb2
Jouni Leino <jouni.leino@siili.com>
2020-07-16 20:29:55 +0200

    Adding superuser

--- management/commands/createsu.py
+++ management/commands/createsu.py
@@ -1,3 +1,4 @@
+"""
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
@@ -7,3 +8,4 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username="admin").exists():
             User.objects.create_superuser("admin", "admin@admin.com", "admin")
+"""
