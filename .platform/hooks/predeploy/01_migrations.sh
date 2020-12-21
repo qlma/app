c221051f8ef7f291204db62b918afa24a8248a47
Jouni Leino <jouni.leino@siili.com>
2020-12-22 00:37:06 +0100

    Heroku setup

--- .platform/hooks/predeploy/01_migrations.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source /var/app/venv/*/bin/activate
-cd /var/app/current
-
-python manage.py makemigrations
-python manage.py createsuperuser
-python manage.py migrate
-python manage.py collectstatic --noinput
