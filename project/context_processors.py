69477a6d4c422bd3951ec290e2f1ec1c6e74a6b0
Jouni Leino <jouni.leino@siili.com>
2021-04-05 19:52:38 +0200

    Adding facility email and phone

--- project/context_processors.py
+++ project/context_processors.py
@@ -4,5 +4,7 @@ def global_settings(request):
     # return any necessary values
     return {
         'FACILITY_NAME': settings.FACILITY_NAME,
-        'FACILITY_LOCATION': settings.FACILITY_LOCATION
+        'FACILITY_ADDRESS': settings.FACILITY_ADDRESS,
+        'FACILITY_EMAIL': settings.FACILITY_EMAIL,
+        'FACILITY_PHONE': settings.FACILITY_PHONE,
     }
